453e539804e275fac177d0edc6667d8bd1440a27
Christina <younglijia7@gmail.com>
2016-01-19 14:43:22 +0800

    Initial Commit

---
+++ MyLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLog
+//
+//  Created by 杨利嘉 on 16/1/19.
+//  Copyright © 2016年 杨利嘉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyLog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyLog
+//
+//  Created by 杨利嘉 on 16/1/19.
+//  Copyright © 2016年 杨利嘉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
