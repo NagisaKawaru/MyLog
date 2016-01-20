99332d4b1f55b511453afed420726321bf1020f8
NagisaKawaru <younglijia7@hotmail.com>
2016-01-20 13:46:34 +0800

    国际化(test)

--- MyLog/HomePageViewController.m
+++ MyLog/HomePageViewController.m
@@ -118,7 +118,16 @@
 }
 
 
-
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    UIAlertController *alert=[UIAlertController alertControllerWithTitle:NSLocalizedStringFromTable(@"alertTitle", @"MultiLanguage", nil) message:NSLocalizedStringFromTable(@"message", @"MultiLanguage", nil) preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *action=[UIAlertAction actionWithTitle:NSLocalizedStringFromTable(@"actionTitle", @"MultiLanguage", nil) style:UIAlertActionStyleDefault handler:nil];
+    
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 
 #pragma mark =================UITableViewDelegate============
